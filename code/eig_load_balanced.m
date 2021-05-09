a175f5d73988b78b1258c44541459c4fe3c4db7f
gcunn <77360181+gcunn@users.noreply.github.com>
2021-05-09 11:22:47 -0400

    Fixed integration bugs (psi basis for blked mats)

--- code/eig_load_balanced.m
+++ code/eig_load_balanced.m
@@ -1,4 +1,4 @@
-function [Vstore, Dstore] = eig_load_balanced(N,blked_matrices)
+function [Vstore, Dstore, psi_out] = eig_load_balanced(N,blked_matrices,psi_in)
 % Solving using blocking and GPU acceleration, with custom load-balancing
 % Timing: Need to solve it more than once, and discard first time due to some
 % messages that prints out due to lack of GUI. Ignore timediscard and
@@ -10,29 +10,59 @@ function [Vstore, Dstore] = eig_load_balanced(N,blked_matrices)
 % parpool('local',gpuDeviceCount("available"));
 
 store = {};
+psi_out = {};
 
 if mod(N, 2) == 0
     store{end+1} = blkdiag(blked_matrices{1:N/2-2});
+    temp = psi_in{1};
+    for i = 2:(N/2-2)
+        temp = cat(1,temp,psi_in{i});
+    end
+    psi_out{end+1} = temp;
+    
     store{end+1} = blked_matrices{N/2-1};
+    psi_out{end+1} = psi_in{N/2-1};
     store{end+1} = blked_matrices{N/2};
+    psi_out{end+1} = psi_in{N/2};
     store{end+1} = blked_matrices{N/2+1};
+    psi_out{end+1} = psi_in{N/2+1};
+    
     store{end+1} = blkdiag(blked_matrices{N/2+2:N-1});
+    temp = psi_in{N/2+2};
+    for i = N/2+3:(N-1)
+        temp = cat(1,temp,psi_in{i});
+    end
+    psi_out{end+1} = temp;
 else
 %     store2{end+1} = blkdiag(store{1:((N-1)/2) - 2}, store{((N-1)/2) + 3:N-1});
 %     store2{end+1} = blkdiag(store{(N-1)/2}, store{((N-1)/2) + 1});
 %     store2{end+1} = blkdiag(store{((N-1)/2) - 1 }, store{((N-1)/2) + 2});   
     store{end+1} = blkdiag(blked_matrices{1:((N-1)/2) - 2});
+    temp = psi_in{1};
+    for i = 2:((N-1)/2) - 2
+        temp = cat(1,temp,psi_in{i});
+    end
+    psi_out{end+1} = temp;
+    
     store{end+1} = blked_matrices{((N-1)/2) - 1 };
+    psi_out{end+1} = psi_in{((N-1)/2) - 1 };
     store{end+1} = blked_matrices{(N-1)/2};
+    psi_out{end+1} = psi_in{((N-1)/2)};
     store{end+1} = blked_matrices{((N-1)/2) + 1};
+    psi_out{end+1} = psi_in{((N-1)/2)+1};
     store{end+1} = blked_matrices{((N-1)/2) + 2};
+    psi_out{end+1} = psi_in{((N-1)/2)+2};
+    
     store{end+1} = blkdiag(blked_matrices{((N-1)/2) + 3:N-1});
-
+    temp = psi_in{((N-1)/2) + 3};
+    for i = ((N-1)/2) + 4:(N-1)
+        temp = cat(1,temp,psi_in{i});
+    end
+    psi_out{end+1} = temp;
 end
 
 Vstore = cell(1,length(store));
 Dstore = cell(1,length(store));
-
 
 % tic;
 % parfor k = 1:length(store)
@@ -49,7 +79,7 @@ parfor k = 1:length(store)
     item = gpuArray(store{k});
     [V,D] = eig(item);
     Vstore{k} = V;
-    Dstore{k} = D;
+    Dstore{k} = diag(D);
 end
 % gpu_bal = toc
 
